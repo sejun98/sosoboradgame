bfd632e2d0fdf87098af7ef91a098a38e9f350a2
sejun98 <tmdfl980520@naver.com>
2023-02-13 20:10:03 +0900

    final

---
+++ jsp/reister_view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>회원가입</title>
+</head>
+<body>
+    <h2>회원가입</h2>
+
+    <label for="">아이디</label>
+    <input type="text">
+
+    <label for="">닉네임</label>
+    <input type="text">
+
+    <label for="">비밀번호</label>
+    <input type="text">
+    
+    <label for="">비밀번호 확인</label>
+    <input type="text">
+
+    <button>저장</button>
+</body>
+</html>
